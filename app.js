5362a4f2ecb631b870f87d6e76f97aff51457d2d
Chris Welch <deadclap893@gmail.com>
2017-05-23 12:38:34 -0400

    Initial Commit

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const komada = require("komada");
 
 komada.start({
-  botToken: "MjY4MzQ1MzIzNjA3MjkzOTUy.C8lTxQ.yVzfFahpJHDTZmAiaz-ZxU5qbCI",
+  botToken: require('../bot_logins.json').pando,
   ownerID: "241216483592634368",
   ownerIDo: "195223544186142727",
   clientID: "268345323607293952",
---
+++ commands/eval.js
@@ -0,0 +1,39 @@
+
+
+exports.run = (client, msg, [code]) => {
+  const content = code
+  const result = new Promise((resolve, reject) => resolve(eval(content)));
+
+return result.then(output => {
+  if (typeof output !== 'string') output = require('util').inspect(output, {
+    depth: 0
+  });
+  if (output.includes(client.token)) output = output.replace(client.token, 'Not for your eyes');
+  if (output.length > 1990) console.log(output), output = 'Too long to be printed (content got console logged)';
+
+  return msg.channel.sendCode('js', output);
+}).catch(err => {
+  console.error(err);
+  err = err.toString();
+
+  if (err.includes(client.token)) err = err.replace(client.token, 'Not for your eyes');
+
+  return msg.channel.sendCode('js', err);
+});
+};
+
+exports.conf = {
+  enabled: true,
+  runIn: ["text", "dm", "group"],
+  aliases: ["ev"],
+  permLevel: 10,
+  botPerms: [],
+  requiredFuncs: [],
+};
+
+exports.help = {
+  name: "eval",
+  description: "Evaluates arbitrary Javascript. Reserved for bot owner.",
+  usage: "<expression:str>",
+  usageDelim: "",
+};
