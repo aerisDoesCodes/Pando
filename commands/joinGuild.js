d9fb194318c4f887d7af326ec7d12ec6210acbb9
Chris Welch <deadclap893@gmail.com>
2017-05-28 11:04:49 -0400

    UGH

--- commands/joinGuild.js
+++ commands/joinGuild.js
@@ -3,20 +3,20 @@ exports.run = (client, msg, [gid]) => {
   // msg.channel.send(args, {reply:msg.author.id})
 
   if (client.guilds.has(gid)) {
-    var guild = client.guilds.get(gid)
+    // var guild = client.guilds.get(gid)
     if (!client.guilds.get(gid).defaultChannel.permissionsFor(client.user).has('CREATE_INSTANT_INVITE')) {
       client.users.get(client.guilds.get(gid).ownerID).sendMessage(`One of my developers is trying to join your server. Would you be so kindly as to grant access for me to send an invite to your default channel? (Automated Message)\n
       Any Questions please join the Bot Support server.`)
     }
       client.guilds.get(gid).defaultChannel.createInvite({maxUses: 1}).then(invite => {
-        var i = invite.code
-        msg.author.send(`Here is your invite to ${guild.name}\n http:\/\/discord.gg/${i}`)
+        var inv = invite.code
+        msg.author.send(`Here is your invite to ${guild.name}\n http:\/\/discord.gg/${inv}`)
         msg.reply(`Check your DMs for the invite!`)
         client.funcs.log(`${msg.author.tag} has generated a single use invite for ${guild.name}. Code: ${i}`)
       })
 
 
-  } else {
+  } else if (!client.guilds.has(gid)) {
     msg.reply(`I am not in the guild\: \'${gid}\' Please try again.`)
   }
 
