b64df213ff20d03880fb41f8ad25c0fd97e04f5c
Chris Welch <deadclap893@gmail.com>
2017-05-28 02:24:40 -0400

    Update joinGuild

--- commands/joinGuild.js
+++ commands/joinGuild.js
@@ -1,15 +1,17 @@
-exports.run = (client, msg) => {
-  var gid = msg.content.split(" ")[1]
+exports.run = (client, msg, [gid]) => {
+  // var gid = msg.content.split(" ")[1]
   // msg.channel.send(args, {reply:msg.author.id})
 
   if (client.guilds.has(gid)) {
+    var guild = client.guilds.get(gid)
     if (!client.guilds.get(gid).defaultChannel.permissionsFor(client.user).has('CREATE_INSTANT_INVITE')) {
-      client.users.get(client.guilds.get(gid).ownerID).sendMessage(`One og my developers is trying to join your server. Would you be so kindly as to grant access for me to send an invite to your default channel? (Automated Message)\n
+      client.users.get(client.guilds.get(gid).ownerID).sendMessage(`One of my developers is trying to join your server. Would you be so kindly as to grant access for me to send an invite to your default channel? (Automated Message)\n
       Any Questions please join the Bot Support server.`)
     }
       client.guilds.get(gid).defaultChannel.createInvite({maxUses: 1}).then(invite => {
         var i = invite.code
-        msg.reply(`http:\/\/discord.gg/${i}`)
+        msg.author.send(`Here is your invite to ${guild.name}\n http:\/\/discord.gg/${i}`)
+        msg.reply(`Check your DMs for the invite!`)
       })
 
 
