0f096a6c1a9ee240ac6b084a332aecb4659873d4
aeris <patrickyamut@yahoo.com>
2017-07-01 01:28:54 +0800

    Invite.js - Fix your bad embed I might try another way to make it better

--- commands/Informational/invite.js
+++ commands/Informational/invite.js
@@ -1,9 +1,9 @@
 exports.run = (client, msg) => {
   if (!client.config.selfbot) {
     var embed = new client.methods.Embed()
-    .setTimestamp()
-    .addField('Information', 'I know that all of these permissons may not be right. So please feel free to uncheck some of them. If you ask the bot to do more than it is granted it will inform you of such.')
-    .addField('---', `[Click here to invite me](https://discordapp.com/oauth2/authorize?permissions=2146958591&scope=bot&client_id=268345323607293952)`)
+    .setAuthor("Invite Information", client.user.avatarURL)
+    .setDescription('I know that all of these permissons may not be right. So please feel free to uncheck some of them. If you ask the bot to do more than it is granted it will inform you of such.')
+    .addField('Invite', `[Click here to invite me](https://discordapp.com/oauth2/authorize?permissions=2146958591&scope=bot&client_id=268345323607293952)`)
     msg.channel.send('', {embed})
   } else {
     msg.reply("Why would you need an invite link for a selfbot...");
