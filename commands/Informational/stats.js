71c0e14f337a0a48f2bd5e5ab74beacff8de27ab
aeris <patrickyamut@yahoo.com>
2017-07-03 17:51:50 +0800

    I just fixed the indent

--- commands/Informational/stats.js
+++ commands/Informational/stats.js
@@ -7,13 +7,13 @@ exports.run = (client, msg) => {
   const duration = moment.duration(client.uptime).format(" D [days], H [hrs], m [mins], s [secs]");
   msg.channel.sendCode("asciidoc", `= STATISTICS =
 
-• Mem Usage  :: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB
-• Uptime     :: ${duration}
-• Users      :: ${client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString()}
-• Servers    :: ${client.guilds.size.toLocaleString()}
-• Channels   :: ${client.channels.size.toLocaleString()}
-• Komada     :: v${komada.version}
-• Discord.js :: v${Discord.version}`);
+  • Mem Usage  :: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB
+  • Uptime     :: ${duration}
+  • Users      :: ${client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString()}
+  • Servers    :: ${client.guilds.size.toLocaleString()}
+  • Channels   :: ${client.channels.size.toLocaleString()}
+  • Komada     :: v${komada.version}
+  • Discord.js :: v${Discord.version}`);
 };
 
 exports.conf = {
--- commands/Informational/whois.js
+++ commands/Informational/whois.js
@@ -5,17 +5,17 @@ require("moment-duration-format");
 exports.run = (client, msg) => {
   let mention = msg.mentions.users.first();
   if(msg.mentions.users.size === 0) {
-  return msg.channel.send("\`❌\` | Please mention a user.")
+    return msg.channel.send("\`❌\` | Please mention a user.")
   }
   msg.channel.sendCode("asciidoc", `= STATISTICS =
 
-• Name       :: ${mention.username}
-• Discrim    :: #${mention.discriminator}
-• User ID    :: ${mention.id}
-• Joined at  :: ${moment(mention.joinedAt).format('ddd MMM Do YYYY')}
-• Status     :: ${mention.presence.game === null ? "No Game" : mention.presence.game.name}
-• Game       :: ${komada.version}
-• Joined Discord :: ${moment(message.mention.createdAt).format('ddd MMM Do YYYY')}`);
+  • Name       :: ${mention.username}
+  • Discrim    :: #${mention.discriminator}
+  • User ID    :: ${mention.id}
+  • Joined at  :: ${moment(mention.joinedAt).format('ddd MMM Do YYYY')}
+  • Status     :: ${mention.presence.game === null ? "No Game" : mention.presence.game.name}
+  • Game       :: ${komada.version}
+  • Joined Discord :: ${moment(message.mention.createdAt).format('ddd MMM Do YYYY')}`);
 };
 
 exports.conf = {
