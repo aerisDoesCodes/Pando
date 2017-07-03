a28233eabcf3259435b5cb3c02eeb4b32d274042
aeris <patrickyamut@yahoo.com>
2017-07-03 18:27:17 +0800

    [setNickname] - Update permLevel (Should be with Bench role)

--- commands/Guild/Admin/setNickname.js
+++ commands/Guild/Admin/setNickname.js
@@ -55,7 +55,7 @@ exports.conf = {
   enabled: true,
   runIn: ['text', 'dm'],
   aliases: ['sn', 'cn', 'setNickname'],
-  permLevel: 10, // I don't know the perm level on this, I might check the perms later.
+  permLevel: 2, // This perm should be with Bench role or a server Owner (Optional `Bench and Owner`). I prefer with Bench role
   botPerms: [],
   requiredFuncs: [],
 }
