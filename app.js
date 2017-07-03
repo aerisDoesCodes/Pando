b3e128022226a811d67e2577fd3b8937703deaa7
Chris Welch <deadclap893@gmail.com>
2017-07-03 03:25:12 -0400

    [app] returned default prefix to io.

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ komada.start({
   ownerID: "241216483592634368",
   ownerIDo: "195223544186142727",
   clientID: "268345323607293952",
-  prefix: "dev.",
+  prefix: "io.",
   sBlackList: serverBlackList,
   aowners: olist,
   clientOptions: {
