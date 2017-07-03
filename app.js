dcc5ca1b38185adc664f7de998169cc09fac1c62
Chris Welch <deadclap893@gmail.com>
2017-07-03 03:22:20 -0400

    [app] Re Hidden Client Token.

--- app.js
+++ app.js
@@ -6,8 +6,7 @@ var serverBlackList = new Array()
 serverBlackList.push('328321964865486850')
 serverBlackList.push('270331157156069376')
 komada.start({
-  // botToken: require('../bot_logins.json').Pando,
-  botToken: "MzEzMDk1MTEwNjE3MjAyNjg4.DDkZzg.wsz4aBEBf6gw0cwgSItDoI-LLSI",
+  botToken: require('../bot_logins.json').Pando,
   ownerID: "241216483592634368",
   ownerIDo: "195223544186142727",
   clientID: "268345323607293952",
