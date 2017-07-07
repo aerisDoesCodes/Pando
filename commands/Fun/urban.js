15aceabc532fd0ab777938bc4ad79734c7cd4536
Chris Welch <deadclap893@gmail.com>
2017-07-07 12:42:06 -0400

    [urban] correct grammar

--- commands/Fun/urban.js
+++ commands/Fun/urban.js
@@ -6,7 +6,7 @@ exports.run = (client, msg) => {
     var embed = new client.methods.Embed()
     .addField(json.word, json.definition)
     .addField('Example', json.example)
-    .addField(`Learn more abour ${json.word}`, `[Click Here to go to Urban Dictionary](http://www.urbandictionary.com/define.php?term=${link})`)
+    .addField(`Learn more about ${json.word}`, `[Click Here to go to Urban Dictionary](http://www.urbandictionary.com/define.php?term=${link})`)
     .setColor(client.funcs.hex(true))
     return msg.channel.send({embed})
   })
