4a9308ea8056670d43de3b5a97b206d470257ad8
Chris Welch <deadclap893@gmail.com>
2017-07-07 12:39:45 -0400

    [urban] setup embed

--- commands/Fun/urban.js
+++ commands/Fun/urban.js
@@ -1,8 +1,14 @@
 const urban = require('urban');
 exports.run = (client, msg) => {
   const args = msg.content.split(' ').slice(1).join(' ');
+  const link = msg.content.split(' ').slice(1).join('+');
   urban(args).first((json) => {
-    console.log(json)
+    var embed = new client.methods.Embed()
+    .addField(json.word, json.definition)
+    .addField('Example', json.example)
+    .addField(`Learn more abour ${json.word}`, `[Click Here to go to Urban Dictionary](http://www.urbandictionary.com/define.php?term=${link})`)
+    .setColor(client.funcs.hex(true))
+    return msg.channel.send({embed})
   })
 
 };
