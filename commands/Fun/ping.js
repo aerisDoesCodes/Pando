bd41fb8284930912d3f5a3af6471a7a893a6226c
aeris <patrickyamut@yahoo.com>
2017-07-12 20:55:05 +0800

    [ping] - Ping command should be a fun command lol

--- commands/Fun/ping.js
+++ commands/Fun/ping.js
@@ -2,7 +2,7 @@ exports.run = async (client, msg) => {
   let pong = ["Pung!", "Peng!", "Pong!", "Pang!"]
   try {
     const message = await msg.channel.sendMessage("Ping?");
-    await message.edit(`${pong} (Roundtrip took: ${message.createdTimestamp - msg.createdTimestamp}ms. Heartbeat: ${Math.floor(client.ping)}ms.)`);
+    await message.edit(pong[Math.floor(Math.random() * pong.length)] + `(Roundtrip took: ${message.createdTimestamp - msg.createdTimestamp}ms. Heartbeat: ${Math.floor(client.ping)}ms.)`);
   } catch (e) {
     client.funcs.log(e, "error");
   }
@@ -19,7 +19,7 @@ exports.conf = {
 
 exports.help = {
   name: "ping",
-  description: "Ping/Pong command. I wonder what this does? /sarcasm",
+  description: "Ping/Pong/Pung/Peng/Pang command. I wonder what this does? /sarcasm",
   usage: "",
   usageDelim: "",
 };
