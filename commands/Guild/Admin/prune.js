f1c19b0774f34b89ba80921f9352ba35bca739da
aeris <patrickyamut@yahoo.com>
2017-07-12 17:15:41 +0800

    [prune] - Added prune command, I don't know on line 26.

---
+++ commands/Guild/Admin/prune.js
@@ -0,0 +1,28 @@
+exports.run = (client, msg) => {
+  let amount = msg.content.split(" ").splice(1, 2).join(" ");
+  if(amount < 1) {
+    return msg.reply("The amount of messages to remove is 1-100.");
+  }
+  if(amount >100) {
+    return msg.reply("Choose a number between 1-100.");
+  }
+  msg.channel.bulkDelete(amount);
+  msg.delete()
+  msg.channel.send("Pruned: " + amount + " Messages").then(m => m.delete(2500));
+};
+
+exports.conf = {
+  enabled: true,
+  runIn: ["text"],
+  aliases: [],
+  permLevel: 2,
+  botPerms: [],
+  requiredFuncs: [],
+};
+
+exports.help = {
+  name: "prune",
+  description: "Prune a number of message on the current channel.",
+  usage: "<int:int>", /* 🤔 I think I am right */
+  usageDelim: " ",
+};
