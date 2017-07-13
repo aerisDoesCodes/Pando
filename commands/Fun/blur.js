156c8e5ecae7a3528f10a3e94c6b51b5fbb22819
aeris <patrickyamut@yahoo.com>
2017-07-13 14:11:11 +0800

    [blur] - Added blur command on Fun module.

---
+++ commands/Fun/blur.js
@@ -0,0 +1,48 @@
+var Jimp = require('jimp')
+exports.run = (client, msg, [user]) => {
+  if (msg.mentions.users.size === 0) {
+    const res = msg.channel.send(':gear: generating...')
+    Jimp.read(msg.author.avatarURL, (err, avatar) => {
+      if (err) return msg.edit('failed to generate.')
+      avatar.blur(5).getBuffer(Jimp.MIME_PNG, (err, buffer) => {
+        msg.delete()
+        msg.channel.send({
+          files: [{
+            attachment: buffer,
+            name: 'bringBack-sendFile.png'
+          }]
+        })
+      })
+    })
+  } else {
+    const res = msg.channel.send(':gear: generating...')
+    Jimp.read(msg.mentions.users.first().avatarURL, (err, avatar) => {
+      if (err) return msg.edit('failed to generate.')
+      avatar.blur(5).getBuffer(Jimp.MIME_PNG, (err, buffer) => {
+        msg.delete()
+        msg.channel.send({
+          files: [{
+            attachment: buffer,
+            name: 'bringBack-sendFile.png'
+          }]
+        })
+      })
+    })
+  }
+};
+
+exports.conf = {
+  enabled: true,
+  runIn: ["text"],
+  aliases: [],
+  permLevel: 0,
+  botPerms: [],
+  requiredFuncs: [],
+};
+
+exports.help = {
+  name: "blur",
+  description: "Blur avatar on user mentioned or your avatar.",
+  usage: "[user:mention]",
+  usageDelim: "",
+};
