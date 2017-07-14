188226562bcb917aa2bc99a2dd1a78d7a26d10a0
aeris <patrickyamut@yahoo.com>
2017-07-14 22:22:14 +0800

    This thing is cancer in komada lol, [fixing usage]

---
+++ commands/Fun/remindme.js
@@ -0,0 +1,32 @@
+const Sherlock = require('sherlockjs');
+const moment = require('moment');
+
+exports.run = async (client, msg, [action, value]) => {
+  const s = Sherlock.parse(msg.content);
+  const relative = s.startDate.getTime() - Date.now();
+  s.eventTitle = s.eventTitle.replace(/^me to ?|^me ?|^to ?/, '');
+  msg.channel.send(`I will remind you to ${s.eventTitle} ${moment().add(relative, 'ms').fromNow()}.`);
+  setTimeout(() => {
+    let final = `${s.eventTitle}`;
+    embed.setAuthor("REMINDER")
+    .setDescription(final)
+    .setTimestamp()
+    msg.author.send('', ${embed}).catch(() => msg.channel.send(`${msg.author} ${final}`));
+  }, relative);
+};
+
+exports.conf = {
+  enabled: true,
+  runIn: ["text"],
+  aliases: [],
+  permLevel: 0,
+  botPerms: [],
+  requiredFuncs: [],
+};
+
+exports.help = {
+  name: "remindme",
+  description: "Set something to remind.",
+  usage: "<seconds|minutes|hours> [str:str]",
+  usageDelim: "",
+};
