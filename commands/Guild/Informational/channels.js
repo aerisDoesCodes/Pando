89be73bfbcad71c74c852c01803175492fd02e71
aeris <patrickyamut@yahoo.com>
2017-07-14 09:55:53 +0800

    line 2, map => array

--- commands/Guild/Informational/channels.js
+++ commands/Guild/Informational/channels.js
@@ -1,5 +1,5 @@
 exports.run = async (client, msg, [type]) => {
-  let channels = msg.guild.channels.map(r => r.name).join(' \n ')
+  let channels = msg.guild.channels.array(r => r.name).join(' \n ')
   let embed = new client.methods.Embed()
   if (!msg.guild.member(client.user).hasPermission("EMBED_LINKS")) {
     return msg.channel.send("`ERROR:` I do not have permission to send Embed, contact administrator to get permission to send embed.");
@@ -7,7 +7,7 @@ exports.run = async (client, msg, [type]) => {
   switch (type) {
     case "list":
     embed.setAuthor(`${msg.guild.name} channels`)
-    .setDescription(`#${channels}`)
+    .setDescription(channels)
     .setColor(client.funcs.hex())
     .setFooter(`There are ${msg.guild.channels.map(r => r.name).size} channels in ${msg.guild.name}`)
     msg.channel.send('', {embed}).catch((err) => {msg.channel.send(`:warning: **An error occurred.**\n${err}`); console.log(err)});
